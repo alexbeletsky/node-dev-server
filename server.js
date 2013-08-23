fb8e2bef1efe85ab0995095da8dd0c38723383ef
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-08-23 19:46:03 +0300

    removed logs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var connect = require('connect');
 connect()
 	.use(connect.static(__dirname))
 	.use(function (req, res, next) {
-		console.log(req.url);
 		fs.createReadStream('index.html').pipe(res);
 	})
 	.listen(8080);
